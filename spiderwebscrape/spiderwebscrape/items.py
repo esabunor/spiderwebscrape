8e58183b987d0c636e116c2336f7233593e9dac2
esabunor <tesabunor@gmail.com>
2017-09-04 23:53:14 +0800

    define first item

--- spiderwebscrape/spiderwebscrape/items.py
+++ spiderwebscrape/spiderwebscrape/items.py
@@ -5,10 +5,8 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
-
-class SpiderwebscrapeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Article(Item):
+    #define the fields for your item here like:
+    title = Field()
